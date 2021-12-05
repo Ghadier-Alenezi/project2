6af56fbe12117127c0f593797a626358f0377fbf
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-12-05 06:14:24 +0300

    update user schema

--- db/models/user.js
+++ db/models/user.js
@@ -1,21 +1,28 @@
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, trim: true, required: true },
-  email: { type: String, trim: true, required: true, unique: true },
-  password: { type: String, trim: true, required: true },
-  img: {type: String, default: "https://ih1.redbubble.net/image.1481363416.9728/st,small,507x507-pad,600x600,f8f8f8.jpg"},
-  age: { type: Number, trim: true },
-  role: [{ type: mongoose.Schema.Types.ObjectId, ref: "Role" }],
-  learnProgrss: [
-    {
-      type: ObjectId,
-      ref: "Lesson",
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, trim: true, required: true },
+    email: { type: String, trim: true, required: true, unique: true },
+    password: { type: String, trim: true, required: true },
+    img: {
+      type: String,
+      default:
+        "https://ih1.redbubble.net/image.1481363416.9728/st,small,507x507-pad,600x600,f8f8f8.jpg",
     },
-  ],
-},{
-  timestamps: true
-});
+    age: { type: Number, trim: true },
+    role: [{ type: mongoose.Schema.Types.ObjectId, ref: "Role" }],
+    learnProgrss: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Lesson",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
